e93b1c42e27e5146375ce7b8c413253e7ed4588f
Joy Udom <judom.2011@gmail.com>
2021-06-24 16:23:22 +0100

    remove duplicate

---
+++ PythonPackage/remove_duplication.py
@@ -0,0 +1,14 @@
+values = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
+
+
+def no_duplicate(duplist):
+    no_duplist = []
+
+    for i in duplist:
+        if i not in no_duplist:
+            no_duplist.append(i)
+
+    return no_duplist
+
+
+print("The new list size is ", len(no_duplicate(values)))
