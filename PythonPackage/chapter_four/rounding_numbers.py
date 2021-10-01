35b5f0b0db49dc7f44ae5b4b29ee267646200bb4
favouredjay <judom.2011@gmail.com>
2021-10-01 13:20:34 +0100

    chapter four exercise

---
+++ PythonPackage/chapter_four/rounding_numbers.py
@@ -0,0 +1,20 @@
+def rounding_integer():
+    return round(13.56449)
+
+
+def rounding_to_the_nearest_tenth():
+    return round(13.56449, 0)
+
+
+def rounding_to_the_nearest_hundredth():
+    return round(13.56449, 1)
+
+
+def rounding_to_the_nearest_thousandth():
+    return round(13.56449, 2)
+
+
+print(rounding_integer())
+print(rounding_to_the_nearest_tenth())
+print(rounding_to_the_nearest_hundredth())
+print(rounding_to_the_nearest_thousandth())
