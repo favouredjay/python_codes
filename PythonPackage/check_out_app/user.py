4f854d6b343e3bfd3b1b6e38632ed3ef03a3b6c5
favouredjay <judom.2011@gmail.com>
2021-10-17 12:43:02 +0100

    update

---
+++ PythonPackage/check_out_app/user.py
@@ -0,0 +1,10 @@
+class user:
+    name_of_cashier = ''
+    name_of_customer = ''
+
+    def get_name_of_customer(self):
+        return self.name_of_customer
+
+    def get_name_of_cashier(self):
+        return self.name_of_cashier
+
