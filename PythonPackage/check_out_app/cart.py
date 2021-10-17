cf979cf7770860ffb7c475d64160bfbc733cfe33
favouredjay <judom.2011@gmail.com>
2021-10-17 16:08:39 +0100

    update

--- PythonPackage/check_out_app/cart.py
+++ PythonPackage/check_out_app/cart.py
@@ -1,3 +1,5 @@
+from typing import overload
+
 from PythonPackage.check_out_app import product
 
 
@@ -18,10 +20,6 @@ def get_items_in_cart(self):
     return self.item_list
 
 
-def find_item(self, item):
-    return self.item_list.index(item)
-
-
 def get_item_by_name(self, name):
     position = self.find_item(name)
     if position >= 0:
@@ -30,35 +28,23 @@ def get_item_by_name(self, name):
         return None
 
 
-def add_items_to_cart(self, name, amount, quantity):
-    item = product
+def add_items_to_cart(self, item=product):
     self.item_list.append(item)
 
 
 def change_item_in_a_cart(self, old_item=product, new_item=product):
-    found_item = self.find_item(old_item)
-    if found_item < 0:
-        print("Choose what's in your cart")
-        return False
-    else:
-        self.item_list.remove(old_item)
-        self.item_list.append(new_item)
-        return True
+    self.item_list.remove(old_item)
+    self.item_list.append(new_item)
 
 
 def remove_items(self, item):
-    position = self.find_item(item)
-    if position <= 0:
-        print(product.name_of_item, " not found")
-        return False
-    self.item_list.remove(position)
-    print(product.name_of_item, " was deleted")
-    return True
+    self.item_list.remove(item)
 
 
 def calculate_total(self):
     for i in range(len(self.item_list)):
-        total = sum.__get__(product.calculate_amount_and_quantity())
+        total = 0.0
+        total += product.calculate_amount_and_quantity()
         return total
 
 
